36a53d02759908375ae52f19086367ca5ccbaa37
losfair <zhy20000919@hotmail.com>
2017-01-01 17:32:35 +0800

    Allow manually set web directory

--- backend/main.js
+++ backend/main.js
@@ -4,25 +4,28 @@ const bodyParser = require("body-parser");
 const resources = require("./resources.js");
 const requestHandlers = require("./requestHandlers.js");
 
-const app = express();
-app.use(express.static("../web"));
-app.use(bodyParser.json());
-
-app.post("/config/sso_url", requestHandlers.onGetSsoUrl);
-app.post("/user/authenticate", requestHandlers.onRequest("userAuthenticate"));
-app.post("/user/check", requestHandlers.onRequest("userCheck"));
-app.post("/video/new", requestHandlers.onRequest("newVideo"));
-app.post("/video/info", requestHandlers.onRequest("getVideoInfo"));
-app.post("/video/count", requestHandlers.onRequest("getVideoCount"));
-app.post("/video/latest", requestHandlers.onRequest("getLatestVideos"));
-app.post("/video/like/new", requestHandlers.onRequest("createVideoLike"));
-app.post("/video/like/count", requestHandlers.onRequest("getVideoLikeCount"));
-app.post("/comment/new", requestHandlers.onRequest("createComment"));
-app.post("/comment/get", requestHandlers.onRequest("getComments"));
-app.post("/comment/count", requestHandlers.onRequest("getCommentCount"));
+let app;
 
 async function run() {
     await resources.init();
+
+    app = express();
+    app.use(express.static(resources.cfg.webDirectory));
+    app.use(bodyParser.json());
+
+    app.post("/config/sso_url", requestHandlers.onGetSsoUrl);
+    app.post("/user/authenticate", requestHandlers.onRequest("userAuthenticate"));
+    app.post("/user/check", requestHandlers.onRequest("userCheck"));
+    app.post("/video/new", requestHandlers.onRequest("newVideo"));
+    app.post("/video/info", requestHandlers.onRequest("getVideoInfo"));
+    app.post("/video/count", requestHandlers.onRequest("getVideoCount"));
+    app.post("/video/latest", requestHandlers.onRequest("getLatestVideos"));
+    app.post("/video/like/new", requestHandlers.onRequest("createVideoLike"));
+    app.post("/video/like/count", requestHandlers.onRequest("getVideoLikeCount"));
+    app.post("/comment/new", requestHandlers.onRequest("createComment"));
+    app.post("/comment/get", requestHandlers.onRequest("getComments"));
+    app.post("/comment/count", requestHandlers.onRequest("getCommentCount"));
+
     app.listen(resources.cfg.listenPort);
 }
 
